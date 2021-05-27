80bcef4266d31d47087b4c83673654a67d0fde2a
hgeaydem <hgeaydem@redhat.com>
2021-05-27 09:27:12 +0200

    fixing wait first cluster

--- files/wait-first-cluster.sh
+++ files/wait-first-cluster.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 export KUBECONFIG=/root/auth-rhacm/kubeconfig
 
-echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (1/5, This can take a while, please sit down and relax)"
+echo -ne "\n[INFO] Waiting for the first cluster to be done installing (1/3, This can take a while, please sit down and relax)"
 while [ ! "`oc status 2>/dev/null |grep -o openshift`" ]; do
    echo -n "."
    sleep 60s
 done
-echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (2/5)"
+echo -ne "\n[INFO] Waiting for the first cluster to be done installing (2/3)"
 while [ "`oc status 2>/dev/null |grep -o openshift`" ]; do
    echo -n "."
    sleep 5s
 done
-echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (3/5)"
+echo -ne "\n[INFO] Waiting for the first cluster to be done installing (3/3)"
 while [ ! "`oc status 2>/dev/null |grep -o openshift`" ]; do
    echo -n "."
    sleep 1s
 done
-echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (4/5)"
-while [ "`oc status 2>/dev/null |grep -o openshift`" ]; do
-   echo -n "."
-   sleep 5s
-done
-echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (5/5 ... We're almost there !)"
-while [ ! "`oc status 2>/dev/null |grep -o openshift`" ]; do
-   echo -n "."
-   sleep 1s
-done
+# echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (4/5)"
+# while [ "`oc status 2>/dev/null |grep -o openshift`" ]; do
+#    echo -n "."
+#    sleep 5s
+# done
+# echo -ne "\n[INFO] Waiting for the rhacm cluster to be done installing (5/5 ... We're almost there !)"
+# while [ ! "`oc status 2>/dev/null |grep -o openshift`" ]; do
+#    echo -n "."
+#    sleep 1s
+# done
